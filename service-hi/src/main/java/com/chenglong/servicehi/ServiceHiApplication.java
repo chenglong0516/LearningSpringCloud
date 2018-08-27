f618075831d6179c0c8873c3efa42f31d629683e
chenglong <chenglong_ustb@163.com>
2018-08-27 14:57:44 +0800

    test

--- service-hi/src/main/java/com/chenglong/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/chenglong/servicehi/ServiceHiApplication.java
@@ -21,6 +21,6 @@ public class ServiceHiApplication {
 	String port;
 	@RequestMapping("/hi")
 	public String home(@RequestParam String name) {
-		return "hi "+name+",i am from port:" +port;
+		return "hi ,"+name+",i am from port:" +port;
 	}
 }
